2bc72ab994fd27c652c052b34d6fb4af4d2676cf
samvesta <92927895+samvesta@users.noreply.github.com>
2021-11-22 14:36:41 +0200

    Local changed line
    Local changed line

--- WebAppTest/WebAppTest/Default.aspx.cs
+++ WebAppTest/WebAppTest/Default.aspx.cs
@@ -14,7 +14,7 @@ namespace WebAppTest
             if(!this.IsPostBack)
             {
 
-                Response.Write("This is my new work added by DEV");
+                Response.Write("This is my new work added by LOCAL");
                
             }
         }
