366ec01b713e78bbb2ad7dfc6917ff26236c03a4
samuelphoenixerp <94829027+samuelphoenixerp@users.noreply.github.com>
2021-11-22 14:18:40 +0200

    Line was changed by 146
    Line was changed by 146

--- WebAppTest/WebAppTest/Default.aspx.cs
+++ WebAppTest/WebAppTest/Default.aspx.cs
@@ -14,7 +14,7 @@ namespace WebAppTest
             if(!this.IsPostBack)
             {
 
-                Response.Write("This is my new work clone");
+                Response.Write("This is my new DEV from 146");
                
             }
         }
